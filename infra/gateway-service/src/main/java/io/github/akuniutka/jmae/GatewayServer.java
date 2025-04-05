3e1dd4936d98bf81f518e165665631c274680b0f
Andrei Kuniutka <akuniutka@gmail.com>
2025-04-05 18:01:27 +0300

    Feat: Add gateway service

---
+++ infra/gateway-service/src/main/java/io/github/akuniutka/jmae/GatewayServer.java
@@ -0,0 +1,12 @@
+package io.github.akuniutka.jmae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServer {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(GatewayServer.class, args);
+    }
+}
